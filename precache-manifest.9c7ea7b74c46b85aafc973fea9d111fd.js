0a6be0d52aad92bc10df25176882f35a58c5a16c
Adit Srivastava <adit48@gmail.com>
2020-08-23 01:57:54 +0530

    update

---
+++ precache-manifest.9c7ea7b74c46b85aafc973fea9d111fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205d83323bd6da4b15cdfcb329a76557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c245b7d276570900e56c",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9ff04e16e919b3a48f98",
+    "url": "/static/js/2.6e0e3d42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e0e3d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c245b7d276570900e56c",
+    "url": "/static/js/main.7ac23ad6.chunk.js"
+  },
+  {
+    "revision": "0304b25de87b70461b45",
+    "url": "/static/js/runtime-main.5f6c0109.js"
+  }
+]);
