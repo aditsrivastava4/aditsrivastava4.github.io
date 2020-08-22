2aac82113ce8bd3a3bf221cc15593412929fc5a3
Adit Srivastava <adit48@gmail.com>
2020-08-23 01:51:14 +0530

    under development

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div style={{
+                "textAlign": "center"
+            }}>
+                <h1 >Under Development</h1>
+                <p>By Adit Srivastava</p>
+            </div>
+        )
+
+    }
+}
+
+export default App;
