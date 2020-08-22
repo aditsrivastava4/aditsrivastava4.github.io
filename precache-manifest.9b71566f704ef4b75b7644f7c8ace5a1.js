008943aa77c60ac51bf1317c13d781cdeb5b841e
Adit Srivastava <adit48@gmail.com>
2020-08-22 21:00:25 +0530

    Initial commit

---
+++ precache-manifest.9b71566f704ef4b75b7644f7c8ace5a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275e26c1bc1140b2e68ddc95d7957ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c0b2d4b627fa4c10f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99c7189615926838947f",
+    "url": "/static/js/2.77c379de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77c379de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c0b2d4b627fa4c10f8",
+    "url": "/static/js/main.0afee7d9.chunk.js"
+  },
+  {
+    "revision": "dcad35ce53814db73077",
+    "url": "/static/js/runtime-main.a7dc1534.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
